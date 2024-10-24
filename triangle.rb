a7ae779dc81761f085b5088c51adfb91ccd55b0e
aaronquijano <ron23.quijano@gmail.com>
2024-10-25 01:43:30 +0800

    Finished the triangle project 2

--- triangle.rb
+++ triangle.rb
@@ -15,13 +15,26 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
-  if a == b && b == c
+  if a < 0 || b < 0 || c < 0 
+    raise TriangleError.new, "Sides can't be less than zero"
+  end
+
+  if a == 0 || b == 0 || c == 0 
+    raise TriangleError.new, "Sides can't be zero"
+  end
+
+  if (a+b <= c || a+c <= b || b+c <= a)
+    raise TriangleError.new, "Hahaha"
+  end
+
+  if a == b && a == c
     :equilateral
   elsif a != b && a != c && b != c
     :scalene
-  else
+  else 
     :isosceles
   end
+
 end
 
 # Error class used in part 2.  No need to change this code.
